21e6ed46cd9c6b239a33165c3791a9229f07d513
kuwaharu <siw23010015@class.siw.ac.jp>
2025-05-13 14:17:41 +0900

    ユーザーモデルを設定するためにAUTH_USER_MODELを追加

--- app/backend/myproject/settings.py
+++ app/backend/myproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# 認証用のユーザーモデルを指定
+AUTH_USER_MODEL = "users.User"
