43e719d857e82c17e46b6ee28bdfe797a96b78ed
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-05-15 11:29:05 +0900

    views.pyにテスト用のAPIエンドポイントを追加

--- app/backend/users/views.py
+++ app/backend/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from django.db.models import F, Value, Sum
+from django.db.models.functions import Coalesce
+from .models import User
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
+from users.exception import BusinessException
+from django.conf import settings
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    TokenRefreshSerializer,
+)
 
-# Create your views here.
+
+class TestView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"})
