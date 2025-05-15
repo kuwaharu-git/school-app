7dd94bd4e245f6bbaa2bcc44c2c6d7ea0f898973
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-05-15 10:51:23 +0900

    settings.pyにJWT認証の設定を追加

--- app/backend/myproject/settings.py
+++ app/backend/myproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "users",
 ]
 
@@ -132,3 +133,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # 認証用のユーザーモデルを指定
 AUTH_USER_MODEL = "users.User"
+
+# JWT認証の設定
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated"
+    ],
+}
