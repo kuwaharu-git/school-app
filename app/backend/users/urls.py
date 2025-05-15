96d23dd749e7cf6afe26840ca98f35e185649d40
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-05-15 13:14:15 +0900

    ユーザー関連のURLパターンを追加し、ログイン、リトライ、ログアウト、パスワード変更のエンドポイントを定義

--- app/backend/users/urls.py
+++ app/backend/users/urls.py
@@ -10,4 +10,12 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("test/", views.TestView.as_view()),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("retry/", views.RetryView.as_view(), name="retry"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path(
+        "change_password/",
+        views.ChangePasswordView.as_view(),
+        name="change_password",
+    ),
 ]
