7a39925a678342bd328e59ab184f9df750437471
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-05-16 22:11:06 +0900

    UserAdminクラスを追加し、ユーザー管理のカスタム設定を追加

--- app/backend/users/admin.py
+++ app/backend/users/admin.py
@@ -1,4 +1,53 @@
 from django.contrib import admin
 from users.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "id",
+        "school_id",
+        "username",
+        "is_initial_password",
+        "is_active",
+        "is_staff",
+        "email",
+        "first_name",
+        "last_name",
+    )
+    readonly_fields = ("date_joined",)
+    list_filter = ("is_active", "is_staff")
+    search_fields = ("school_id", "username", "email")
+    ordering = ("-date_joined",)
+    filter_horizontal = ()
+    fieldsets = (
+        (None, {"fields": ("school_id", "username", "password")}),
+        ("Personal Info", {"fields": ("email", "first_name", "last_name")}),
+        (
+            "Permissions",
+            {"fields": ("is_active", "is_staff", "is_initial_password")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "school_id",
+                    "username",
+                    "password1",
+                    "password2",
+                    "is_initial_password",
+                    "is_active",
+                    "is_staff",
+                    "email",
+                    "first_name",
+                    "last_name",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
