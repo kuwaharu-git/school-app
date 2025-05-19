70acaec6087feae47a6ba673d8fb1a764da95928
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-05-19 16:03:10 +0900

    axiosのインポートをcustomAxiosに変更し、axios関連の型定義を追加

--- app/frontend/src/app/page.tsx
+++ app/frontend/src/app/page.tsx
@@ -1,17 +1,19 @@
 'use client';
 import React, { useEffect, useState } from "react";
-import axios from "./plugins/axios";
+import axios from "./plugins/customAxios";
+import { AxiosResponse, AxiosError } from "axios";
 
 export default function Page() {
   const [text, setText] = useState("");
 
   useEffect(() => {
     axios.get("/api/users/test")
-      .then((res) => {
+      .then((res: AxiosResponse) => {
         setText(res.data.message);
       })
-      .catch((error) => {
+      .catch((error: AxiosError) => {
         setText("データの取得に失敗しました");
+        console.error("データの取得に失敗:", error);
       });
   }, []);
 
@@ -24,10 +26,10 @@ export default function Page() {
         onClick={() => {
           axios
             .post("/api/users/logout")
-            .then((res) => {
+            .then(() => {
               window.location.href = "/login";
             })
-            .catch((error) => {
+            .catch((error: AxiosError) => {
               console.error("ログアウト失敗:", error);
             });
         }}
