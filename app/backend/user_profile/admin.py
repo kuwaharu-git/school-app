bbb873218998040b30d3f0c6e927150c92715542
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-05-25 09:45:39 +0900

    管理画面にユーザープロファイル関連のモデルを登録

--- app/backend/user_profile/admin.py
+++ app/backend/user_profile/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from user_profile.models import (
+    Profiles,
+    Languages,
+    Frameworks,
+    SocialMedias,
+    UserLanguages,
+    UserFrameworks,
+    UserSocialMedias,
+)
 
 # Register your models here.
+admin.site.register(Profiles)
+admin.site.register(Languages)
+admin.site.register(Frameworks)
+admin.site.register(SocialMedias)
+admin.site.register(UserLanguages)
+admin.site.register(UserFrameworks)
+admin.site.register(UserSocialMedias)
