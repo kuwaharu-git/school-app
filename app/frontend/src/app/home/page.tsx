2ed6add5b82df59f93e5ce990f4a5e7d4c05dcf0
kuwaharu <141981256+kuwaharu-git@users.noreply.github.com>
2025-08-19 10:30:18 +0900

    不要なインポートを削除

--- app/frontend/src/app/home/page.tsx
+++ app/frontend/src/app/home/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { noRedirectCustomAxios } from "@/lib/customAxios";
 import { AxiosResponse, AxiosError } from "axios";
 import { Header } from "@/components/Header";
